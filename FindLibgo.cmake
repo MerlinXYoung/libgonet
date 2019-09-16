6211225793e75f455cca175fa146fabba03baf69
MerlinXYoung <merlin_young@163.com>
2019-09-16 09:23:51 +0800

    build

--- FindLibgo.cmake
+++ FindLibgo.cmake
@@ -1,20 +1,29 @@
 
 find_path(Libgo_INCLUDE_DIR
-    libgo/coroutine.h
-    PATHS /usr/include /usr/local/include ${Libgo_DIR}/include
+libgo/libgo.h
+PATHS /usr/include /usr/local/include ${Libgo_DIR}/include
 )
 
 find_library(Libgo_LIBRARIES
-    libgo 
-    PATHS /usr/lib /usr/local/lib ${Libgo_DIR}/lib 
+libgo
+PATHS /usr/lib /usr/local/lib ${Libgo_DIR}/lib
 )
-
-if(NOT Libgo_INCLUDE_DIR) 
-    set(Libgo_NOTFOUND ON)
-    message(WARNING "libgo not found! include")
-elseif(NOT Libgo_LIBRARIES)
-    set(Libgo_NOTFOUND ON)  
-    message(WARNING "libgo not found! lib")
+message(STATUS "Libgo:${Libgo_INCLUDE_DIR} ${Libgo_LIBRARIES}")
+string(COMPARE NOTEQUAL "${Libgo_INCLUDE_DIR}"  "" Libgo_INCLUDE_DIR-NOTFOUND)
+string(COMPARE NOTEQUAL "${Libgo_LIBRARIES}"  "" Libgo_LIBRARIES-NOTFOUND)
+#message(STATUS "Libgo:${Libgo_INCLUDE_DIR-NOTFOUND} ${Libgo_LIBRARIES-NOTFOUND}")
+if(Libgo_INCLUDE_DIR-NOTFOUND)
+message(WARNING "Libgo_INCLUDE_DIR-NOTFOUND")
+set(Libgo-NOTFOUND ON)
+elseif(Libgo_LIBRARIES-NOTFOUND)
+message(WARNING "Libgo_LIBRARIES-NOTFOUND")
+set(Libgo-NOTFOUND ON)
 else()
-    string(REPLACE "liblibgo.a" "" ${Libgo_LIBRARIES} Libgo_LIBRARY_DIR)
+set(Libgo_FOUND ON)
+string(REPLACE "/liblibgo.a" "" Libgo_LIBRARY_DIR ${Libgo_LIBRARIES})
+string(COMPARE EQUAL ${Libgo_LIBRARY_DIR} ${Libgo_LIBRARIES} _EQUAL)
+if(_EQUAL)
+string(REPLACE "/liblibgo.so" "" Libgo_LIBRARY_DIR ${Libgo_LIBRARIES})
+endif()
+message(STATUS "Libgo_LIBRARY_DIR:${Libgo_LIBRARY_DIR}")
 endif()
